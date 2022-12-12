6bba56bb0e4f2a4098c7f021468178e69083ce41
Julia BATORO <jbatoro@e3r4p3.clusters.42paris.fr>
2022-12-12 16:34:52 +0100

    ok

---
+++ 00_vector/Vector.hpp
@@ -0,0 +1,55 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <iostream>
+
+// namespace ft 
+// https://www.geeksforgeeks.org/how-to-implement-our-own-vector-class-in-c/
+namespace ft
+{
+    template< class T >
+    struct allocator;
+    template <typename T, typename Alloc = std::allocator T> // use allocator here 
+    class vector 
+    {
+        public:
+            // fill constructor
+            // constructor par copie
+            // constructor par range 
+            // operateur de surchage 
+
+        private:
+
+        /* 
+        // MEMBERS FUNCTIONS    
+
+        ( 1 ) Constructors  
+
+        ( 2 ) Destructor    
+
+        ( 3 ) Overload operator=    
+
+        ( 4 ) Iterators 
+
+        ( 5 ) Capacity  
+
+        ( 6 ) Element access    
+
+        ( 7 ) Modifiers 
+
+        ( 8 ) Allocator     
+
+        // NON MEMBERS FUNTIONS OVERLOAD    
+
+        */ 
+        };
+
+}
+
+#endif
---
+++ 00_vector/main.cpp
@@ -0,0 +1,6 @@
+#include "Vector.hpp"
+
+int main()
+{
+
+}
